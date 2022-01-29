0bc749bcda3a7bb32e417b3138dd47c0a9e60532
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-01-29 00:30:13 +0000

    displaying weather

--- assets/script.js
+++ assets/script.js
@@ -8,3 +8,17 @@ var currentUVEl = document.querySelector(".UV-index");
 var TodayEl = document.querySelector("#currentDay");
 var TimeEl = document.querySelector(".time");
 var forecastEls = document.querySelectorAll(".five-day");
+
+
+var APIKey = "bd0d0c0b9ffe1286395e4abb56d49400";
+
+function getWeather() {
+
+    fetch ( "https://api.openweathermap.org/data/2.5/weather?q=" + formInput.value + "&appid=" + APIKey)
+   .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) { console.log(data)
+      currentHumidityEl.innerHTML = "Humidity: " + data.main.humidity + "%"
+      currentTempEl.innerHTML = "Temperature: " + data.main.temp + "&#176F"
+      currentWindEl.innerHTML = "Wind Speed: " + data.wind.speed + " MPH" })}
